3af5d67b84d14c3b913a7e94760b5745cceb7409
Tajwar <tajwaraashab@gmail.com>
2021-07-16 22:53:22 +0600

    Deserialization added

--- code.go
+++ code.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"gol/model"
+	"io/ioutil"
+	"log"
 
 	"github.com/golang/protobuf/proto"
 )
@@ -14,9 +16,22 @@ func main() {
         Model: "corolla",
         Release: 1999,
     }
-    serialize, err := proto.Marshal(car)
-    if err == nil {
-        fmt.Println(serialize)
-    } 
+    // serialize data
+    serialized, err := proto.Marshal(car)
+    if err != nil {
+        log.Fatal("Marshalling Error: ", err)
+    }
+    ioutil.WriteFile("car.data", serialized, 0644)
+    
+    // deserialize
+    car2 := &model.Car{}
+    
+    err = proto.Unmarshal(serialized, car2)
+    if err != nil {
+        log.Fatal("Unmarshal error: ", err)
 
+    }
+    fmt.Println(car2.Brand)
+    fmt.Println(car2.Model)
+    fmt.Println(car2.Release)
 }
